46f4436bb3ee5f58a7c972894305458feab85a11
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-22 17:51:39 +0100

    created myFn function

--- src/components/MainTodo.tsx
+++ src/components/MainTodo.tsx
@@ -2,10 +2,13 @@ import InputTodo from "./InputTodo";
 import ListTodo from "./ListTodo";
 
 export default function MainTodo() {
+  const myFn = () => {
+    alert("myFn called!");
+  };
   return (
     <>
       <h1>Get Things Done</h1>
-      <InputTodo />
+      <InputTodo alertFn={myFn} />
       <ListTodo />
     </>
   );
